12378cdf53e52682571fa64008f4a055027954ee
yyeonjju <llilyha123@gmail.com>
2024-06-04 16:44:01 +0900

    Initial Commit

---
+++ MediaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaProject
+//
+//  Created by 하연주 on 6/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
